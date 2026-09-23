c3aa9230d4773512fae07c3407d0ccc8059f8eca
2231119156 <2231119156@CPES-3B1037-P06.ku.edu.kw>
2026-09-23 11:33:05 +0300

    lab 1 push 2

--- firstlab.java
+++ firstlab.java
@@ -4,6 +4,9 @@ public class firstlab {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+System.out.println("hello world");
+
+
 	}
 
 }

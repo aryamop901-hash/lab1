2c72e36cc652b2425d3de696580f225f76f1fe91
ARY <aryamop901@gmail.com>
2026-09-23 11:43:22 +0300

    Update firstlab.java

--- firstlab.java
+++ firstlab.java
@@ -5,6 +5,9 @@ public class firstlab {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
 System.out.println("hello world");
+		System.out.println("hello world");
+
+		
 
 
 	}
